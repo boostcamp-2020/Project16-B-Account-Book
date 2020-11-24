217549802089067bace11cd71be2aa506eaecdef
Yejikwon <yeeji77@naver.com>
2020-11-24 14:02:51 +0900

    [BE] feat: cors 설정 추가
    app.js에 cors 설정을 추가했습니다.
    
    origin은 8080 포트를 사용하는 frontend 서버에 cors 허용이 가능하도록 하기위해 cors 설정을 추가했습니다.

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,26 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const indexRouter = require('./route/index');
+const cors = require('cors');
 const bodyParser = require('koa-bodyparser');
+
+const indexRouter = require('./route/index');
 
 require('dotenv').config();
 require('./startup/db')();
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+};
 
 const app = new Koa();
 const router = new Router();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser());
+app.use(cors(corsOptions));
 router.use('', indexRouter.routes());
 app.use(router.routes()).use(router.allowedMethods());
+
 app.on('error', (err) => {
   console.log('server error', err);
 });

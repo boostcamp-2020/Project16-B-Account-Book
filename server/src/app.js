c4ba990259e51c95a5afc2bf570fe81d2c0c955a
Yejikwon <yeeji77@naver.com>
2020-11-24 13:37:56 +0900

    [BE] feat: post test
    body-parser가 잘 적용되는지 확인하고, post test를 진행해보았습니다.

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const indexRouter = require('./route/index');
+const bodyParser = require('koa-bodyparser');
 
 require('dotenv').config();
 require('./startup/db')();
@@ -9,9 +10,9 @@ const app = new Koa();
 const router = new Router();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser());
 router.use('', indexRouter.routes());
 app.use(router.routes()).use(router.allowedMethods());
-
 app.on('error', (err) => {
   console.log('server error', err);
 });

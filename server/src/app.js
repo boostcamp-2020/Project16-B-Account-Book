072865c654ec7e0ef36af578e1f809a313ada47e
Yejikwon <yeeji77@naver.com>
2020-11-24 14:21:42 +0900

    [BE] feat: cors 설정 추가
    app.js에 @koa/cors 모듈을 통해 koa cors 설정을 추가했습니다.

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const cors = require('cors');
+const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 
 const indexRouter = require('./route/index');

77c84a7a09a6e8b2dad6ff4d43422ae1e9bfdd1c
gyim1345 <gyim1345@gmail.com>
2020-11-25 19:08:24 +0900

    [BE] feat: index.jsx 수정
    Store 와 router 적용

---
+++ client/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import store from './store';
+
+ReactDOM.render(
+  (
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  ),
+  document.getElementById('root'),
+);

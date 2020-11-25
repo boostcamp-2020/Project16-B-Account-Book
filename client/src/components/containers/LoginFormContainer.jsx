bad98eba0f557e6814c1fc0ede56c323c81e211f
gyim1345 <gyim1345@gmail.com>
2020-11-25 19:06:20 +0900

    [FE] feat: 구조틀 잡기 위한 파일 생성
    page, container, presentational component 생성 및 실험용 코드 작성

---
+++ client/src/components/containers/LoginFormContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import LoginForm from '../presentational/LoginForm';
+
+import { useDispatch } from 'react-redux';
+
+import {
+  loader
+} from '../../slice';
+
+const LoginFormContainer = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = ({ test }) => {
+    dispatch(loader({ test }));
+  }
+
+  return (
+    <>
+      <div>LoginFormContainer</div>
+      <LoginForm 
+        onClick={handleClick}
+      />
+    </>
+  )
+};
+
+
+export default LoginFormContainer;
---
+++ client/src/components/presentational/LoginForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginForm = ({ onClick }) => {
+  
+  return (
+    <>
+      <div>LoginForm</div>
+      <button onClick={()=>{onClick({test: Date.now()})}}>aa</button>
+    </>
+  )
+}
+
+export default LoginForm;

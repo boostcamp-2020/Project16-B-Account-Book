5fb9273aff8fa883ba00d1b5ca4d9966323e16e1
JC Chung <jotace422@gmail.com>
2020-11-26 16:51:23 +0900

    [FE] refactor: color.js를 추가하여 자주 사용하는 색상 관리
    styled-components에서 반복적으로 사용되는 색상들을 변수로 관리하도록 color.js 추가

---
+++ client/public/color.js
@@ -0,0 +1,10 @@
+const color = {
+  line: "#EEEEEE",
+  fontNormal: "#777777",
+  fontBold: "#222222",
+  fontLightBold: "#616161",
+  pageBackground: "#FDFDFE",
+  boxShadow: "1px 1px 5px 0px #e7e7e7",
+};
+
+export default color;

2ab1717ef1d4c7608f731f6bc7d9ea341a566149
Yejikwon <yeeji77@naver.com>
2020-11-26 16:56:42 +0900

    [FE] chore: webpack alias 설정
    자주 쓰이거나 depth가 길어질 수 있는 디렉토리에 alias를 설정했습니다.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,50 +1,56 @@
-const path = require("path");
-const Dotenv = require("dotenv-webpack");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const port = process.env.PORT || 8080;
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
-  resolve: { extensions: [".js", ".jsx"] },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@public': path.resolve(__dirname, './public'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 
-  entry: { app: ["babel-polyfill", "./src/index"] },
+  entry: { app: ['babel-polyfill', './src/index'] },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
     new CleanWebpackPlugin(),
     new RefreshWebpackPlugin(),
     new Dotenv({
-      path: "./.env",
+      path: './.env',
     }),
   ],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
   devServer: {
-    host: "localhost",
+    host: 'localhost',
     historyApiFallback: true,
     port: port,
     hot: true,

d7147ee1e95009fcdc0e5ab9e972d47b3aeb1117
JC Chung <jotace422@gmail.com>
2020-11-26 16:58:21 +0900

    [FE] feat: GlobalStyle 적용 및 Layout 컴포넌트 추가
    배경색, 여백, 패딩 등을 초기화 하는 GlobalStyle 추가
    헤더와 사이드바 컴포넌트를 묶어서 모든 페이지에 적용할 수 있는 Layout 컴포넌트 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,33 @@
 import React from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
-import LoginPage from './pages/LoginPage';
-import AccountBookPage from './pages/AccountBookPage';
-import AnalysisPage from './pages/AnalysisPage';
-import CalendarPage from './pages/CalendarPage';
-import CategoryPage from './pages/CategoryPage';
-import DashboardPage from './pages/DashboardPage';
-import NotFoundPage from './pages/NotFoundPage';
-import PaymentMethodPage from './pages/PaymentMethodPage';
-import SettingPage from './pages/SettingPage';
-import TransactionPage from './pages/TransactionPage';
+import LoginPage from "./pages/LoginPage";
+import AccountBookPage from "./pages/AccountBookPage";
+import AnalysisPage from "./pages/AnalysisPage";
+import CalendarPage from "./pages/CalendarPage";
+import CategoryPage from "./pages/CategoryPage";
+import DashboardPage from "./pages/DashboardPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import PaymentMethodPage from "./pages/PaymentMethodPage";
+import SettingPage from "./pages/SettingPage";
+import TransactionPage from "./pages/TransactionPage";
+import Layout from "./components/presentational/common/Layout";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #ffffff;
+    margin: 0;
+    padding: 0;
+    padding-bottom: 100px;
+  }
+`;
 
 const App = () => {
   return (
     <>
-      <div>Hello React</div>
+      <GlobalStyle />
+      <Layout />
       <Switch>
         <Route exact path="/" component={LoginPage} />
         {/* <Route path="/account-book" component={AccountBookPage} /> */}
@@ -26,7 +38,7 @@ const App = () => {
         {/* <Route path="/payment-method" component={PaymentMethodPage} /> */}
         {/* <Route path="/setting" component={SettingPage} /> */}
         {/* <Route path="/transaction" component={TransactionPage} /> */}
-        {/* <Route component={NotFoundPage} /> */}
+        {<Route component={NotFoundPage} />}
       </Switch>
     </>
   );

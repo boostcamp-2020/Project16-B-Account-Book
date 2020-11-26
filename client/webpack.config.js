dbb77290f76ec3a5b3eadd7bcad632f32d270a53
gyim1345 <gyim1345@gmail.com>
2020-11-27 00:06:53 +0900

    [FE] CI: webpack.config.js 플러그인 추가
    ProvidePlugin 사용 하여 모든 파일에 react가 기본적으로 import 되게끔 적용

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,55 +3,59 @@ const Dotenv = require('dotenv-webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const webpack = require('webpack');
 
 const port = process.env.PORT || 8080;
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '@public': path.resolve(__dirname, './public'),
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  entry: { app: ['babel-polyfill', './src/index'] },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new CleanWebpackPlugin(),
-    new RefreshWebpackPlugin(),
-    new Dotenv({
-      path: './.env',
-    }),
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  devServer: {
-    host: 'localhost',
-    historyApiFallback: true,
-    port: port,
-    hot: true,
-  },
+	mode: 'development',
+	devtool: 'inline-source-map',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+		alias: {
+			'@public': path.resolve(__dirname, './public'),
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	entry: { app: ['babel-polyfill', './src/index'] },
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+		new CleanWebpackPlugin(),
+		new RefreshWebpackPlugin(),
+		new Dotenv({
+			path: './.env',
+		}),
+		new webpack.ProvidePlugin({
+			React: 'react',
+		}),
+	],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+		filename: 'bundle.js',
+	},
+	devServer: {
+		host: 'localhost',
+		historyApiFallback: true,
+		port: port,
+		hot: true,
+	},
 };

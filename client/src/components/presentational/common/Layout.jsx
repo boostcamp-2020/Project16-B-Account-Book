25ea4dcd6370f586e4a084e5db2f3de17f55e08f
JC Chung <jotace422@gmail.com>
2020-11-27 05:28:34 +0900

    [FE] feat: layout 템플릿 작성
    header, sidebar, content 세 부분으로 템플릿 구획화

--- client/src/components/presentational/common/Layout.jsx
+++ client/src/components/presentational/common/Layout.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+
 import Header from './Header';
+import Sidebar from './Sidebar';
+import color from '@public/color';
 
 const LayoutWrapper = styled.div``;
 
-const Layout = () => {
+const ContentWrapper = styled.div`
+  position: absolute;
+  top: 61px;
+  left: 251px;
+  padding: 50px 20px;
+  width: 100%;
+  height: 100%;
+  background-color: ${color.pageBackground};
+  transition: 850ms;
+
+  @media (max-width: 767px) {
+    transform: translate(-231px, 0px);
+  }
+`;
+
+const Layout = ({ children: content }) => {
   return (
     <>
       <LayoutWrapper>
         <Header />
+        <Sidebar />
+        <ContentWrapper>{content}</ContentWrapper>
+        <div className="layout-body"></div>
       </LayoutWrapper>
     </>
   );

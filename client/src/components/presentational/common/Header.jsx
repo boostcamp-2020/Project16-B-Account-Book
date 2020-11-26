c87b6da87bb628a5d9e56bd6db4556ae95e1cf67
Yejikwon <yeeji77@naver.com>
2020-11-26 19:33:54 +0900

    [FE] refactor: #9 Header & Sidebar 분리
    Header 컴포넌트가 너무 커져서 Sidebar 관련된 부분과 나눴습니다.

--- client/src/components/presentational/common/Header.jsx
+++ client/src/components/presentational/common/Header.jsx
@@ -3,12 +3,11 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { IconContext } from 'react-icons';
 import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
 
 import svg from '@public/svg';
 import color from '@public/color';
-import { SidebarList } from './SidebarList';
 import UserMenuDropDown from './UserMenuDropDown';
+import SideBar from './Sidebar';
 
 const HeaderWrapper = styled.div`
   display: flex;
@@ -64,71 +63,6 @@ const MenuBars = styled.div`
   }
 `;
 
-const MavMenu = styled.div`
-  .nav-menu {
-    z-index: 10;
-    background-color: ${color.backgroundColor};
-    border-right: 1px solid ${color.line};
-    width: 250px;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    position: fixed;
-    top: 0;
-    left: -100%;
-    transition: 850ms;
-  }
-
-  .nav-menu.active {
-    left: 0;
-    transition: 350ms;
-  }
-
-  .nav-text {
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    padding: 8px 0 8px 16px;
-    list-style: none;
-    height: 60px;
-  }
-
-  .nav-text a {
-    text-decoration: none;
-    color: #f5f5f5;
-    font-size: 15px;
-    width: 95%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    padding: 0 10px;
-    border-radius: 4px;
-  }
-
-  .nav-text a:hover {
-    background-color: ${color.hoverColor};
-  }
-
-  .nav-menu-items {
-    width: 100%;
-    padding-left: 0;
-  }
-
-  .navbar-toggle {
-    background-color: ${color.backgroundColor};
-    width: 100%;
-    height: 80px;
-    display: flex;
-    justify-content: start;
-    align-items: center;
-  }
-`;
-
-const Span = styled.span`
-  color: ${color.fontBold};
-  margin-left: 16px;
-`;
-
 const Header = () => {
   const [sidebar, setSidebar] = useState(false);
   const [userMenu, setUserMenu] = useState(false);
@@ -170,30 +104,7 @@ const Header = () => {
             {userMenu && <UserMenuDropDown setUserMenu={setUserMenu} />}
           </UserMenu>
 
-          <MavMenu>
-            <div className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-              <ul className="nav-menu-items" onClick={showSidebar}>
-                <li className="navbar-toggle">
-                  <MenuBars>
-                    <Link to="#">
-                      <AiIcons.AiOutlineClose size={20} />
-                    </Link>
-                  </MenuBars>
-                </li>
-
-                {SidebarList.map((item, index) => {
-                  return (
-                    <li key={'sidebar' + index} className={item.cName}>
-                      <Link to={item.path}>
-                        {item.icon}
-                        <Span>{item.title}</Span>
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </MavMenu>
+          <SideBar sidebar={sidebar} showSidebar={showSidebar} />
         </HeaderWrapper>
       </IconContext.Provider>
     </>

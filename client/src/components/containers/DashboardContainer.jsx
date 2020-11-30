f1e05430631c2382fd9a43d47d104227292f1949
gyim1345 <gyim1345@gmail.com>
2020-11-30 19:25:59 +0900

    [FE] feat: Dashboard에 component 추가
    DashboardTextExpense 를 추가 하여 수입 지출을 text로 볼수 있게 추가
    text는 카드 별로 출력 해야 함으로 transaction을 맞게 파싱화 후 props로 전달

--- client/src/components/containers/DashboardContainer.jsx
+++ client/src/components/containers/DashboardContainer.jsx
@@ -1,14 +1,25 @@
-import DashboardVisualExpense from '../presentational/DashboardVisualExpense';
-
 import { useSelector } from 'react-redux';
+
+import DashboardVisualExpense from '../presentational/DashboardVisualExpense';
+import DashboardTextExpense from '../presentational/DashboardTextExpese';
 
 const DashboardContainer = () => {
   const transactions = useSelector((state) => state.transactions);
+
+  const transactionByCard = transactions.reduce((acc, cur) => {
+    const index = acc.findIndex((item) => item.card === cur.card);
+    if (acc[index]) {
+      acc[index].cost += cur.cost;
+      return acc;
+    }
+    return [...acc, { card: cur.card, cost: cur.cost }];
+  }, []);
 
   return (
     <>
       <div>DashboardContainer</div>
       <DashboardVisualExpense transactions={transactions} />
+      <DashboardTextExpense transactions={transactionByCard} />
     </>
   );
 };

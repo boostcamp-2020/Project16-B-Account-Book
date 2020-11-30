7542d46ae49f77240a00ccdb4615c2c10e38aa53
gyim1345 <gyim1345@gmail.com>
2020-11-30 12:50:23 +0900

    [BE] feat: TransactionController 추가
    적절한 request를 받으면 user에 대한 transaction을 response로 돌려줌
    에러가 발생하면 error 돌려줌

---
+++ server/src/controller/transaction.controller.js
@@ -0,0 +1,24 @@
+const transactionService = require('../service/transaction.service');
+
+const TransactionController = {
+  fetchTransaction: async (ctx) => {
+    try {
+      //TODO: oauth 미들웨어로 유저 인증 및 데이터 주입.
+      const name = ctx.request.body.user || 'gibong';
+      const provider = ctx.request.body.provider || 'naver';
+      const providerId = ctx.request.body.providerId || '12345';
+
+      const transactions = await transactionService.getUserTransactions(
+        name,
+        provider,
+        providerId
+      );
+
+      ctx.body = transactions;
+    } catch (err) {
+      ctx.body = 'error';
+    }
+  },
+};
+
+module.exports = TransactionController;

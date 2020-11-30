41dd660183f232df03fcd3016bf8552aa602ea57
Yejikwon <yeeji77@naver.com>
2020-11-30 21:53:16 +0900

    [FE] feat: #23 카드 추가버튼 구현
    카드 추가버튼을 구현했습니다.
    
    클릭하면 카드를 등록할 수 있는 모달이 나타나게 됩니다.

---
+++ client/src/components/presentational/payment/AddButton.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import * as FcIcons from 'react-icons/fc';
+
+import PaymentModal from './PaymentModal';
+
+const AddPaymentBtn = styled.div`
+  width: 84%;
+  display: flex;
+  flex-direction: row-reverse;
+  cursor: pointer;
+  border: none;
+  outline: none;
+`;
+
+const AddButton = () => {
+  const AddCard = (cardName) => {
+    console.log(cardName);
+  };
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <AddPaymentBtn onClick={handleShow}>
+        <FcIcons.FcPlus
+          size={40}
+          style={{
+            filter: `drop-shadow(4px 2px 2px rgba(0, 0, 0, 0.173))`,
+          }}
+        />
+      </AddPaymentBtn>
+
+      <PaymentModal isOpen={show} close={handleClose} AddCard={AddCard} />
+    </>
+  );
+};
+
+export default AddButton;

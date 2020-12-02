0adc4ae3194923fbd7a0dc4b86d4ca3f3d79156e
gyim1345 <gyim1345@gmail.com>
2020-12-02 23:21:16 +0900

    [FE] style: svg 추가
    github svg 추가
    naver svg 추가

---
+++ client/src/svgs/NaverSvg.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const StyledSvg = styled.svg`
+  height: 18px;
+  width: 18px;
+  display: block;
+`;
+
+export default ({ background }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+    fill="#1EC800"
+  >
+    <title>soc020 - Regular</title>
+    <g id="soc020">
+      <g id="Regular-10" data-name="Regular">
+        <g id="layer1">
+          <path
+            id="path2830"
+            d="M3.2,4.632,3.128,27.3l8.912.036.036-9.167L11.489,15.3,19.924,27.3l8.948.073L28.835,4.705l-8.948-.036.183,9.351.587,3.337L12.076,4.669,3.2,4.632Z"
+          />
+        </g>
+      </g>
+    </g>
+  </svg>
+);

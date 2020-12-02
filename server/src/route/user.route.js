dc735d79b19334c6be5e33a3b07665afa08cda24
gyim1345 <gyim1345@gmail.com>
2020-12-02 14:35:07 +0900

    [BE] feat: user.route에 post /login 추가
    Post /login 요청 시에 githubOAuth 미들웨어 거쳐서 controller의 login 실행

--- server/src/route/user.route.js
+++ server/src/route/user.route.js
@@ -1,8 +1,10 @@
 const Router = require('koa-router');
+const githubOAuth = require('../../middleware/githubOAuth');
 const router = new Router();
 const userController = require('../controller/user.controller');
 
 router.get('/', userController.test);
 router.post('/', userController.update);
+router.post('/login', githubOAuth, userController.login);
 
 module.exports = router;

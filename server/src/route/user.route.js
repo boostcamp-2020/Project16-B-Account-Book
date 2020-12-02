39d91dc4cbe4bcd23d8813be6a0aa46bbd1a9e32
gyim1345 <gyim1345@gmail.com>
2020-12-02 16:42:09 +0900

    [BE] feat: 네이버 github oauth api url 분리 및 Oauth 적용
    네이버 url은 post /naverlogin
    github url은 post /githublogin
    으로 변경.
    각각 맞는 Oauth 미들웨어 적용

--- server/src/route/user.route.js
+++ server/src/route/user.route.js
@@ -1,10 +1,12 @@
 const Router = require('koa-router');
 const githubOAuth = require('../../middleware/githubOAuth');
+const naverOAuth = require('../../middleware/naverOAuth');
 const router = new Router();
 const userController = require('../controller/user.controller');
 
 router.get('/', userController.test);
 router.post('/', userController.update);
-router.post('/login', githubOAuth, userController.login);
+router.post('/githublogin', githubOAuth, userController.login);
+router.post('/naverlogin', naverOAuth, userController.login);
 
 module.exports = router;

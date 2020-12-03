880b6b448fb7616251340ca56e4218b17164e172
Yejikwon <yeeji77@naver.com>
2020-12-03 19:38:11 +0900

    [FE] chore: webpack alias 설정
    C:/Program Files/Git/src/paymentSlice의 alias를 설정했습니다.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
+      '@paymentSlice': path.resolve(__dirname, './src/paymentSlice'),
       '@slice': path.resolve(__dirname, './src/slice'),
       '@presentational': path.resolve(
         __dirname,

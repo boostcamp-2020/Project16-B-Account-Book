850895eb0a2c1d3ba618e203df2aea004a351bb2
JC Chung <jotace422@gmail.com>
2020-12-04 09:36:43 +0900

    [FE] fix: 파일이름 변경
    CardContainer 에서 CardList로 변경
    presentational component이기 때문에 container 대신 list를 사용

---
+++ client/src/components/presentational/category_tag/CardList.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import icon from '@public/icon';
+
+import CategoryCard from './CategoryCard';
+import TagCard from './TagCard';
+
+const CardListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  .inactive {
+    display: none;
+  }
+`;
+
+const AddTagBtn = styled.div`
+  width: fit-content;
+  display: flex;
+  flex-direction: row-reverse;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  position: absolute;
+  top: -50px;
+  right: 15px;
+`;
+
+const CardList = ({
+  data,
+  navMenu,
+  onClickAdd,
+  onClickChange,
+  onClickDelete,
+}) => {
+  const [addMode, setAddMode] = useState(false);
+
+  const renderCategoryList = (categories) => {
+    return categories.map(({ icon, title, description }, index) => {
+      return (
+        <CategoryCard
+          key={'card' + index}
+          iconName={icon}
+          title={title}
+          description={description}
+        />
+      );
+    });
+  };
+
+  const handleVisibility = (tag) => {
+    if (addMode) return false;
+    return tag.length ? false : true;
+  };
+
+  const renderTagList = (tags) => {
+    return tags.concat(['']).map((tag, index) => {
+      return (
+        <TagCard
+          key={'tag' + index}
+          iconName="tagDefault"
+          title={tag}
+          onClickAdd={onClickAdd}
+          onClickChange={onClickChange}
+          onClickDelete={onClickDelete}
+          hidden={handleVisibility(tag)}
+          setAddMode={setAddMode}
+        />
+      );
+    });
+  };
+
+  const onClickAddTag = () => setAddMode(!addMode);
+
+  return (
+    <>
+      <CardListWrapper>
+        {navMenu === 'tag' ? renderTagList(data) : renderCategoryList(data)}
+        {navMenu === 'tag' && (
+          <AddTagBtn onClick={onClickAddTag}>{icon.addBtn}</AddTagBtn>
+        )}
+      </CardListWrapper>
+    </>
+  );
+};
+
+export default CardList;

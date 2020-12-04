15d8c079f6db7613ca2282e521be520d4ac57074
JC Chung <jotace422@gmail.com>
2020-12-04 09:49:01 +0900

    [FE] feat: 카테고리/태그 페이지 진입시 dispatch 코드 추가
    처음 카테고리/태그 페이지에 진입할 경우 store에 tags를 load 해오도록 구현

--- client/src/pages/CategoryTagPage.jsx
+++ client/src/pages/CategoryTagPage.jsx
@@ -1,19 +1,26 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Navigation from '../components/presentational/category_tag/Navigation';
-import CardContainer from '../components/presentational/category_tag/CardContainer';
-import dummy from '../components/presentational/category_tag/CategoryTagDummy';
+import CardList from '../components/presentational/category_tag/CardList';
+import TagContainer from '../components/containers/TagContainer';
+import { loadTag } from '@slice';
+import categories from '../components/presentational/category_tag/categories';
 
 const CategoryTagPage = () => {
   const [navMenu, setNavMenu] = useState('category');
+  const dispatch = useDispatch();
+
+  dispatch(loadTag({ accountBookId: '5fc713abd120a78e5c18216d' }));
 
   return (
     <>
       <Navigation navMenu={navMenu} setNavMenu={setNavMenu} />
-      <CardContainer
-        dummy={navMenu === 'tag' ? dummy.tag : dummy.category}
-        navMenu={navMenu}
-      />
+      {navMenu === 'category' ? (
+        <CardList data={categories} navMenu={navMenu} />
+      ) : (
+        <TagContainer navMenu={navMenu} />
+      )}
     </>
   );
 };

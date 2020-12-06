21db4eb47c42df01eb758356319c86d4b10dc351
gyim1345 <gyim1345@gmail.com>
2020-12-06 23:21:35 +0900

    [FE] feat: add date calulator on specific click
    change date matching the clicked button

--- client/src/components/presentational/transaction/TransactionDate.jsx
+++ client/src/components/presentational/transaction/TransactionDate.jsx
@@ -4,8 +4,32 @@ const StyledDiv = styled.div``;
 
 const TransactionDate = ({ date, updateDateHandler }) => {
   const handleClick = (type, amount) => {
-    const newDate = Date.now();
+    const newDate = calculateDate(date, type, amount);
     updateDateHandler({ date: newDate });
+  };
+
+  const calculateDate = (currentDate, type, difference) => {
+    let month = currentDate.month;
+    let year = currentDate.year;
+    if (type === 'year') {
+      year += difference;
+    }
+
+    if (type === 'month') {
+      if (month + difference === 13) {
+        month = 1;
+        year += 1;
+      }
+      if (month + difference === 0) {
+        month = 12;
+        year -= 1;
+      }
+      if (year === currentDate.year) {
+        month += difference;
+      }
+    }
+
+    return { ...currentDate, year, month };
   };
 
   return (

1768368bd3f8541bf3d2e6694729bd2fd61c55e1
JC Chung <jotace422@gmail.com>
2020-12-07 08:58:23 +0900

    [FE] refactor: 태그 정보를 로드하는 부분을 TagContainer에서 처리하도록 수정
    redux 관련 logic은 Container쪽에서 처리해야하므로 CategoryTagPage에서 분리하여 TagContainer로 이동

--- client/src/pages/CategoryTagPage.jsx
+++ client/src/pages/CategoryTagPage.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
 
 import Navigation from '../components/presentational/category_tag/Navigation';
 import CardList from '../components/presentational/category_tag/CardList';
 import TagContainer from '../components/containers/TagContainer';
-import { loadTag } from '@slice';
 import categories from '../components/presentational/category_tag/categories';
 
 const CategoryTagPage = () => {
   const [navMenu, setNavMenu] = useState('category');
-  const dispatch = useDispatch();
-
-  dispatch(loadTag({ accountBookId: '5fc713abd120a78e5c18216d' }));
 
   return (
     <>

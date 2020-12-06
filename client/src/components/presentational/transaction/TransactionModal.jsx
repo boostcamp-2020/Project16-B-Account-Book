8db44022c90893f9e2ce1e22d073b17676513fc7
gyim1345 <gyim1345@gmail.com>
2020-12-06 22:00:42 +0900

    [FE] feat: Add modal
    used material ui library for modal

---
+++ client/src/components/presentational/transaction/TransactionModal.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const TransactionModal = ({ openModalStatus, setOpenModalStatus }) => {
+  const classes = useStyles();
+
+  const handleClose = () => {
+    setOpenModalStatus(false);
+  };
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={openModalStatus || false}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openModalStatus}>
+          <div className={classes.paper}>hello</div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+};
+
+export default TransactionModal;

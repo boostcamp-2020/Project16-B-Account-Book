6beb0fc27818e7c9c418272c58e0fd0447920e1e
JC Chung <jotace422@gmail.com>
2020-12-07 09:00:38 +0900

    [FE] style: css 수정 및 코드 정리
    사용하지 않는 styled-components 삭제
    리스트가 0개일 경울 화면이 깨질 수 있어 width 값을 줌

--- client/src/components/presentational/common/Layout.jsx
+++ client/src/components/presentational/common/Layout.jsx
@@ -4,14 +4,12 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import color from '@public/color';
 
-const LayoutWrapper = styled.div``;
-
 const ContentWrapper = styled.div`
   position: absolute;
   top: 61px;
   left: 251px;
   padding: 50px 20px;
-  max-width: 972px;
+  width: 972px;
   height: 100%;
   background-color: ${color.pageBackground};
   transition: 850ms;
@@ -25,12 +23,10 @@ const ContentWrapper = styled.div`
 const Layout = ({ children: content }) => {
   return (
     <>
-      <LayoutWrapper>
-        <Header />
-        <Sidebar />
-        <ContentWrapper>{content}</ContentWrapper>
-        <div className="layout-body"></div>
-      </LayoutWrapper>
+      <Header />
+      <Sidebar />
+      <ContentWrapper>{content}</ContentWrapper>
+      <div className="layout-body"></div>
     </>
   );
 };

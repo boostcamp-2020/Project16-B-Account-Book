7971bed4fac389743c62a580eec044c16f08a378
Yejikwon <yeeji77@naver.com>
2020-12-08 17:59:10 +0900

    [FE] refactor: #72 #74 CalendarContainer 리팩토링
    이벤트 관리를 form에서 진행하기 위해 리팩토링을 진행했습니다.

--- client/src/components/containers/CalendarContainer.jsx
+++ client/src/components/containers/CalendarContainer.jsx
@@ -1,33 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
 import CalendarForm from '@presentational/calendar/CalendarForm';
-import { loadPayment } from '@slice';
-
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
 
 const CalendarContainer = () => {
-  const dispatch = useDispatch();
-  const calendarInfo = useSelector((state) => state.calendarInfo);
-
-  const [selectedDate, handleDateChange] = useState(new Date());
-
   return (
     <>
-      <CalendarForm calendarInfo={calendarInfo} />
+      <CalendarForm />
     </>
   );
 };

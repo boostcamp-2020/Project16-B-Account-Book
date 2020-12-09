3af8d6aeefc001ed750cdba719b13db67709ea0c
gyim1345 <gyim1345@gmail.com>
2020-12-09 17:01:23 +0900

    [FE] feat: add reset state feature
    Removes all states

--- client/src/slice.js
+++ client/src/slice.js
@@ -188,9 +188,9 @@ export function login({ code, state }) {
       userInfo = result.userInfo;
     }
 
-    console.log;
+    console.log(accessToken);
+    console.log(userInfo);
     localStorage.setItem('accessToken', accessToken);
-    localStorage.setItem('userInfo', JSON.stringify(userInfo));
     dispatch(setAccessToken(accessToken));
     dispatch(setUserInfo(userInfo));
   };

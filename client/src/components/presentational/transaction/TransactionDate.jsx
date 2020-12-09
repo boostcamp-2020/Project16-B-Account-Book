20f54fccd55992313a1fdf5ddb3f88d679021361
gyim1345 <gyim1345@gmail.com>
2020-12-10 02:55:27 +0900

    [FE] style: change arrow design
    Use icons

--- client/src/components/presentational/transaction/TransactionDate.jsx
+++ client/src/components/presentational/transaction/TransactionDate.jsx
@@ -1,6 +1,21 @@
 import styled from 'styled-components';
+import icon from '@public/icon';
+
+const DateSelectSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2vh;
+`;
 
 const StyledDiv = styled.div``;
+const StyledButton = styled.button`
+  border: none;
+  background: none;
+  outline: none;
+  :hover {
+    cursor: pointer;
+  }
+`;
 
 const TransactionDate = ({ date, updateDateHandler }) => {
   const handleClick = (type, amount) => {
@@ -34,16 +49,30 @@ const TransactionDate = ({ date, updateDateHandler }) => {
 
   return (
     <>
-      <StyledDiv>
-        <button onClick={() => handleClick('year', -1)}> {`<`} </button>
-        year {date.year}
-        <button onClick={() => handleClick('year', 1)}> {`>`} </button>
-      </StyledDiv>
-      <StyledDiv>
-        <button onClick={() => handleClick('month', -1)}> {`<`} </button>
-        month {date.month}
-        <button onClick={() => handleClick('month', 1)}> {`>`} </button>
-      </StyledDiv>
+      <DateSelectSection>
+        <StyledDiv>
+          <StyledButton onClick={() => handleClick('year', -1)}>
+            {' '}
+            {icon.leftArrow}{' '}
+          </StyledButton>
+          year {date.year}
+          <StyledButton onClick={() => handleClick('year', 1)}>
+            {' '}
+            {icon.rightArrow}{' '}
+          </StyledButton>
+        </StyledDiv>
+        <StyledDiv>
+          <StyledButton onClick={() => handleClick('month', -1)}>
+            {' '}
+            {icon.leftArrow}{' '}
+          </StyledButton>
+          month {date.month}
+          <StyledButton onClick={() => handleClick('month', 1)}>
+            {' '}
+            {icon.rightArrow}{' '}
+          </StyledButton>
+        </StyledDiv>
+      </DateSelectSection>
     </>
   );
 };

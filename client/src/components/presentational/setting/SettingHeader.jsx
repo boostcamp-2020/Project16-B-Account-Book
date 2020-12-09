7c27dd04ddce1e58fb44559a2fbf1fbacce0c710
Yejikwon <yeeji77@naver.com>
2020-12-10 01:39:34 +0900

    [FE] feat: #91 셋팅 페이지 로그아웃 버튼 디자인
    로그아웃 로직 적용은 merge되면 추가할 예정입니다.

---
+++ client/src/components/presentational/setting/SettingHeader.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  text-align: center;
+  padding: 0.5em;
+  position: relative;
+  font-family: 'Noto Sans KR', sans-serif;
+`;
+
+const Logout = styled.div`
+  position: absolute;
+  right: 3em;
+  top: 1em;
+`;
+
+const ItemContainer = styled.div`
+  font-size: 1.3rem;
+
+  img {
+    margin-left: 10px;
+    margin-right: 8px;
+    border-radius: 50% !important;
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+  }
+`;
+
+const SettingHeader = ({ userInfo, onLogout }) => {
+  return (
+    <HeaderContainer>
+      <Logout>
+        <Button variant="outlined" onClick={onLogout}>
+          로그아웃 😥
+        </Button>
+      </Logout>
+      <ItemContainer>
+        <img src={userInfo.imageURL} />
+        <div>
+          <b>{userInfo.name}님,</b> 안녕하세요 👏
+        </div>
+      </ItemContainer>
+    </HeaderContainer>
+  );
+};
+
+export default SettingHeader;

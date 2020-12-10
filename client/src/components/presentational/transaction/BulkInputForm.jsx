3dbc8c5a7f3e3377db428f2e75043565118f6583
gyim1345 <gyim1345@gmail.com>
2020-12-11 03:25:49 +0900

    [FE] feat: bulk insert form 작성
    csv로 얻은 다수 데이터를 form에 출력 되게끔 만듬

---
+++ client/src/components/presentational/transaction/BulkInputForm.jsx
@@ -0,0 +1,50 @@
+import { Fragment } from 'react';
+
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+`;
+
+const BulkInputForm = ({
+  bulkInsert,
+  setBulkInsert,
+  bulkInsertTransactionHandler,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(bulkInsert);
+    bulkInsertTransactionHandler({ transactions: bulkInsert });
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        {bulkInsert.map((dataObject, i) => {
+          const keys = Object.keys(dataObject);
+          const values = Object.values(dataObject);
+
+          return (
+            <Fragment key={`dataObj-${i}`}>
+              <StyledDiv>
+                {keys.map((key, index) => {
+                  return (
+                    <Fragment key={`keys-${index}`}>
+                      {/* {key}: <input type="text" value={values[index]}></input> */}
+                      <div>
+                        {key}: {values[index]}
+                      </div>
+                    </Fragment>
+                  );
+                })}
+              </StyledDiv>
+            </Fragment>
+          );
+        })}
+        <button type="submit">submit</button>
+      </form>
+    </>
+  );
+};
+
+export default BulkInputForm;

a16b17ead6f2ee7a021b5595d35d635a662fe943
gyim1345 <gyim1345@gmail.com>
2020-12-14 14:16:45 +0900

    [FE] feat: linechart 수입 지출 필터 및 반응형 추가
    체크박스 로 수입 지출 필터링
    반응형 추가

--- client/src/components/presentational/transaction/TransactionLineChart.jsx
+++ client/src/components/presentational/transaction/TransactionLineChart.jsx
@@ -1,3 +1,7 @@
+import { Checkbox } from '@material-ui/core';
+import { useState } from 'react';
+import styled from 'styled-components';
+
 import {
   ResponsiveContainer,
   LineChart,
@@ -8,6 +12,13 @@ import {
   CartesianGrid,
   Legend,
 } from 'recharts';
+
+const Box = styled.div`
+  width: 45vw;
+  height: 45vw;
+  min-width: 250px;
+  min-height: 250px;
+`;
 
 const changeTransaction = (transactions) => {
   const transactionWithDateKey = transactions.map((transaction) => {
@@ -41,27 +52,62 @@ const changeTransaction = (transactions) => {
 };
 
 const TransactionLineChart = ({ currentDateTransactions }) => {
+  const [isExpense, setIsExpense] = useState(true);
+  const [isIncome, setIsIncome] = useState(true);
+
+  const handleExpenseChange = () => {
+    setIsExpense(!isExpense);
+  };
+
+  const handleIncomeChange = () => {
+    setIsIncome(!isIncome);
+  };
+
   const transactions = changeTransaction(currentDateTransactions);
   return (
-    <LineChart
-      width={600}
-      height={300}
-      data={transactions}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    >
-      <XAxis dataKey="name" />
-      <YAxis />
-      <CartesianGrid strokeDasharray="3 3" />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="지출"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
+    <>
+      지출
+      <Checkbox
+        label="지출"
+        checked={isExpense}
+        onChange={handleExpenseChange}
+        style={{ color: '#8884d8' }}
       />
-      <Line type="monotone" dataKey="수입" stroke="#82ca9d" />
-    </LineChart>
+      수입
+      <Checkbox
+        label="수입"
+        checked={isIncome}
+        onChange={handleIncomeChange}
+        style={{ color: '#82ca9d' }}
+      />
+      <Box>
+        <ResponsiveContainer>
+          <LineChart
+            width={600}
+            height={300}
+            data={transactions}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <XAxis dataKey="name" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Legend />
+            {isExpense && (
+              <Line
+                type="monotone"
+                dataKey="지출"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+            )}
+            {isIncome && (
+              <Line type="monotone" dataKey="수입" stroke="#82ca9d" />
+            )}
+          </LineChart>
+        </ResponsiveContainer>
+      </Box>
+    </>
   );
 };
 

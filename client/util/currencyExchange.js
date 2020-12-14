ca3202755551fb0dcb7b3e3d47ec30fed231c7a0
gyim1345 <gyim1345@gmail.com>
2020-12-14 17:57:17 +0900

    [FE] feat: update currencyExchange
    화폐 심볼로 currency 확인 하는 기능 추가

--- client/util/currencyExchange.js
+++ client/util/currencyExchange.js
@@ -1,8 +1,27 @@
-const currencyExchange = (amount, currency) => {
-  if (exchangeCurrencyToWon[currency]) {
-    return Math.floor(amount * exchangeCurrencyToWon[currency]);
+const currencyExchange = (cost, currency) => {
+  const exchangeRate = getExchangeRate(
+    currency || getCurrencyFromCostText(cost)
+  );
+  if (exchangeRate) {
+    return Math.floor(Number(cost.replace(/[^\d]/g, '')) * exchangeRate);
   }
-  return amount;
+  return cost.replace(/[^\d]/g, '');
+};
+
+const getCurrencyFromCostText = (cost) => {
+  return currencySign[cost.replace(/[0-9]/g, '')];
+};
+
+const getExchangeRate = (currency) => {
+  return exchangeCurrencyToWon[currency];
+};
+
+const currencySign = {
+  $: 'USD',
+  '€': 'EUR',
+  '₽': 'RUB',
+  元: 'CNY',
+  円: 'JPY',
 };
 
 const exchangeCurrencyToWon = {

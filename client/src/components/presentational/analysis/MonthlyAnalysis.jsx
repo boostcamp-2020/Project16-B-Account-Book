00dc62cebc8aa0f03602571dcac32d19be220511
JC Chung <jotace422@gmail.com>
2020-12-15 09:56:07 +0900

    [FE] feat: 보고서 페이지 분석항목 추가
    원별 지출 그래프, 분석내역 추가

---
+++ client/src/components/presentational/analysis/MonthlyAnalysis.jsx
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+
+import color from '@public/color';
+
+const MonthlyAnalysisWrapper = styled.div`
+  border: 1.5px solid ${color.line};
+  background-color: white;
+  padding-top: 0.6rem;
+  padding-left: 1rem;
+  margin-left: 1rem;
+  flex-grow: 100;
+  @media (max-width: 767px) {
+    max-width: 18rem;
+    margin-top: 1rem;
+    margin-left: 0;
+    margin-right: 1rem;
+  }
+`;
+const Title = styled.div`
+  color: #353535;
+  font-size: 1rem;
+  font-weight: bold;
+  margin-bottom: 1.5rem;
+`;
+const Content = styled.div``;
+const Item = styled.div`
+  margin-bottom: 1rem;
+`;
+const Category = styled.div`
+  color: #808080;
+`;
+const Data = styled.span`
+  color: #ffc41f;
+  font-size: 1.5rem;
+`;
+const Unit = styled.span`
+  color: #ffc41f;
+  font-size: 1rem;
+`;
+
+const getLastNElements = (arr, n) => arr.slice(arr.length - n);
+
+const getCostSum = (transactions) => {
+  return transactions.reduce((acc, cur) => {
+    return acc + cur['월별'];
+  }, 0);
+};
+
+const MonthlyAnalysis = ({ title, transactions }) => {
+  const last3Months = getLastNElements(transactions, 3);
+  const last6Months = getLastNElements(transactions, 6);
+  const last12Months = getLastNElements(transactions, 12);
+
+  return (
+    <>
+      <MonthlyAnalysisWrapper>
+        <Title>{title}</Title>
+        <Content>
+          <Item>
+            <Category>3개월 평균 지출:</Category>
+            <Data>
+              {getCostSum(last3Months)}
+              <Unit> 원</Unit>
+            </Data>
+          </Item>
+          <Item>
+            <Category>6개월 평균 지출 :</Category>
+            <Data>
+              {getCostSum(last6Months)}
+              <Unit> 원</Unit>
+            </Data>
+          </Item>
+          <Item>
+            <Category>12개월 평균 지출:</Category>
+            <Data>
+              {getCostSum(last12Months)}
+              <Unit> 원</Unit>
+            </Data>
+          </Item>
+        </Content>
+      </MonthlyAnalysisWrapper>
+    </>
+  );
+};
+
+export default MonthlyAnalysis;
---
+++ client/src/components/presentational/analysis/MonthlyExpenditure.jsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import {
+  ResponsiveContainer,
+  Bar,
+  BarChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
+
+import color from '@public/color';
+
+const ChartWrapper = styled.div`
+  border: 1.5px solid ${color.line};
+  background-color: white;
+  padding: 0.6rem 1rem;
+  width: 38rem;
+  height: fit-content;
+`;
+
+const ChartTitle = styled.div`
+  color: #353535;
+  font-size: 1rem;
+  font-weight: bold;
+  margin-left: 0.5rem;
+  margin-bottom: 0.5rem;
+`;
+const AnnualExpenditure = ({ title, transactions }) => {
+  return (
+    <>
+      <ChartWrapper>
+        <ChartTitle>{title}</ChartTitle>
+        <ResponsiveContainer width="100%" height={400}>
+          <BarChart
+            data={transactions}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="월별" fill="#FFC41F" />
+          </BarChart>
+        </ResponsiveContainer>
+      </ChartWrapper>
+    </>
+  );
+};
+
+export default AnnualExpenditure;

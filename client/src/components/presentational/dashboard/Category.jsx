379684523c483288caf89f52607b11cdc4ac66b9
gyim1345 <gyim1345@gmail.com>
2020-12-16 22:28:52 +0900

    [FE] feat: dashboard category
    대시보드에 카테고리 소비별 보고서 링크 + 이미지 띄우기

---
+++ client/src/components/presentational/dashboard/Category.jsx
@@ -0,0 +1,43 @@
+import coffeeImage from '@public/img/coffee.jpeg';
+import squirrel from '@public/img/squirrel.jpeg';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 0.3em;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.26);
+  border-color: lightgray;
+  /* max-width: fit-content; */
+  width: 30vw;
+  margin: 0px 0px 1% 1%;
+`;
+
+const Text = styled.span`
+  overflow-wrap: break-word;
+  padding: 2%;
+`;
+
+const Category = ({ image, text, redirectCategory }) => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push({
+      pathname: '/analysis',
+      state: { type: 'category', name: redirectCategory },
+    });
+  };
+
+  return (
+    <>
+      <Box onClick={handleClick}>
+        <img src={image} width={'100%'} height={'80%'} />
+        <Text>{text}</Text>
+      </Box>
+    </>
+  );
+};
+
+export default Category;

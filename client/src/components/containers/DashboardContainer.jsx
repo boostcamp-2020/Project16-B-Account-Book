699b6643f216e5dd32b09cd0979fbfdf70469657
gyim1345 <gyim1345@gmail.com>
2020-12-16 22:41:30 +0900

    [FE] feat: Add default value and category section
    Add Category section and default value when transaction is empty

--- client/src/components/containers/DashboardContainer.jsx
+++ client/src/components/containers/DashboardContainer.jsx
@@ -5,11 +5,21 @@ import styled from 'styled-components';
 import DashboardVisualExpense from '../presentational/dashboard/DashboardVisualExpense';
 import DashboardTextExpense from '../presentational/dashboard/DashboardTextExpense';
 import { loadTransactions } from '@slice';
+import CategorySection from '../presentational/dashboard/CategorySection';
+
 const StyledDiv = styled.div`
   display: flex;
+  flex-direction: column;
+
   & > * {
     margin-right: 1rem;
+    margin-bottom: 5%;
   }
+`;
+
+const MonthlyAnalysis = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
 const MediaTextExpense = styled.div`
@@ -46,14 +56,49 @@ const DashboardContainer = () => {
     ];
   }, []);
 
+  const parseData = (transactions) => {
+    return transactions
+      .reduce((acc, cur) => {
+        if (cur.type === '수입') {
+          return acc;
+        }
+        const index = acc.findIndex((item) => item.name === cur.category);
+        if (acc[index]) {
+          acc[index].value += cur.cost;
+          return acc;
+        }
+        return [...acc, { name: cur.category, value: cur.cost }];
+      }, [])
+      .sort((a, b) => {
+        if (a.cost > b.cost) {
+          return 1;
+        }
+        if (a.cost < b.cost) {
+          return -1;
+        }
+        return 1;
+      });
+  };
+
+  const transactionByCategory = parseData(transactions);
+
   return (
     <>
       <div>DashboardContainer</div>
       <StyledDiv>
-        <DashboardVisualExpense transactions={transactions} />
-        <MediaTextExpense>
-          <DashboardTextExpense transactions={transactionByCard} />
-        </MediaTextExpense>
+        <MonthlyAnalysis>
+          {transactions[0] ? (
+            <>
+              <DashboardVisualExpense transactions={transactionByCategory} />
+              <MediaTextExpense>
+                <DashboardTextExpense transactions={transactionByCard} />
+              </MediaTextExpense>
+            </>
+          ) : (
+            <div> No transactions! please move to 수입/지출 내역 tab</div>
+          )}
+        </MonthlyAnalysis>
+        <CategorySection transaction={transactionByCategory} />
       </StyledDiv>
     </>
   );

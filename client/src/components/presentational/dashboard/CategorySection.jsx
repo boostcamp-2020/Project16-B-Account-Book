df627c66a7bcca843fc5b1dfe8bb05e0d42570c0
gyim1345 <gyim1345@gmail.com>
2020-12-17 12:56:10 +0900

    [FE] style: 사진 변경
    사용되는 사진 변경 및 천단위 , 붙이기

--- client/src/components/presentational/dashboard/CategorySection.jsx
+++ client/src/components/presentational/dashboard/CategorySection.jsx
@@ -1,23 +1,9 @@
-import styled from 'styled-components';
-
 import Category from './Category';
-import squirrel from '@public/img/squirrel.jpeg';
-import coffeeImage from '@public/img/coffee.jpeg';
-import beer from '@public/img/beer.jpg';
-
-const Section = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  width: 90vw;
-
-  @media (max-width: 967px) {
-    transform: translate(-0vw, 8%);
-  }
-  @media (max-width: 768px) {
-    transform: scale(0.5) translate(-25vw, -36%);
-  }
-`;
+import beer2 from '@public/img/beer2.jpg';
+import 식사 from '@public/img/식사.jpeg';
+import 마트 from '@public/img/마트.jpg';
+import coffee2 from '@public/img/coffee2.png';
+import onlineShopping from '@public/img/online-shopping.jpg';
 
 const categorySection = ({ transactions }) => {
   const transactionObject = {};
@@ -27,43 +13,47 @@ const categorySection = ({ transactions }) => {
 
   return (
     <>
-      <Section>
-        {transactionObject['술/유흥'] && (
-          <Category
-            image={beer}
-            text={`술/유흥 ${transactionObject['술/유흥']}원`}
-            redirectCategory={'술/유흥'}
-          />
-        )}
-        {transactionObject['식사'] && (
-          <Category
-            image={squirrel}
-            text={`식사 ${transactionObject['식사']}원`}
-            redirectCategory={'식사'}
-          />
-        )}
-        {transactionObject['카페/간식'] && (
-          <Category
-            image={coffeeImage}
-            text={`카페/간식 ${transactionObject['카페/간식']}원`}
-            redirectCategory={'카페/간식'}
-          />
-        )}
-        {transactionObject['생활/마트'] && (
-          <Category
-            image={coffeeImage}
-            text={`생활/마트 ${transactionObject['생활/마트']}원`}
-            redirectCategory={'생활/마트'}
-          />
-        )}
-        {transactionObject['온라인쇼핑'] && (
-          <Category
-            image={coffeeImage}
-            text={`온라인쇼핑 ${transactionObject['온라인쇼핑']}원`}
-            redirectCategory={'온라인쇼핑'}
-          />
-        )}
-      </Section>
+      {transactionObject['술/유흥'] && (
+        <Category
+          image={beer2}
+          text={`술/유흥 ${transactionObject['술/유흥'].toLocaleString()}원`}
+          redirectCategory={'술/유흥'}
+        />
+      )}
+      {transactionObject['식사'] && (
+        <Category
+          image={식사}
+          text={`식사 ${transactionObject['식사'].toLocaleString()}원`}
+          redirectCategory={'식사'}
+        />
+      )}
+      {transactionObject['카페/간식'] && (
+        <Category
+          image={coffee2}
+          text={`카페/간식 ${transactionObject[
+            '카페/간식'
+          ].toLocaleString()}원`}
+          redirectCategory={'카페/간식'}
+        />
+      )}
+      {transactionObject['생활/마트'] && (
+        <Category
+          image={마트}
+          text={`생활/마트 ${transactionObject[
+            '생활/마트'
+          ].toLocaleString()}원`}
+          redirectCategory={'생활/마트'}
+        />
+      )}
+      {transactionObject['온라인쇼핑'] && (
+        <Category
+          image={onlineShopping}
+          text={`온라인쇼핑 ${transactionObject[
+            '온라인쇼핑'
+          ].toLocaleString()}원`}
+          redirectCategory={'온라인쇼핑'}
+        />
+      )}
     </>
   );
 };

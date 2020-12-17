15a96510c8e9aeeb5a47212110d7b066466bfd98
gyim1345 <gyim1345@gmail.com>
2020-12-17 20:44:51 +0900

    [FE] style: css 수정
    text css 추가

--- client/src/components/presentational/dashboard/Category.jsx
+++ client/src/components/presentational/dashboard/Category.jsx
@@ -9,15 +9,35 @@ const Box = styled.div`
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.26);
   border-color: lightgray;
   width: 30vw;
+  height: 30vw;
   margin: 0px 0px 1% 1%;
   @media (max-width: 967px) {
     width: 45vw;
+    height: 45vw;
+  }
+  &:hover {
+    cursor: pointer;
   }
 `;
 
 const Text = styled.span`
   overflow-wrap: break-word;
   padding: 2%;
+  @media (max-width: 967px) {
+    width: 45vw;
+    font-size: 2.5vw;
+  }
+`;
+
+const ExtraText = styled.span`
+  font-size: 12px;
+  font-weight: 100;
+  padding: 0 2%;
+  overflow: hidden;
+  @media (max-width: 967px) {
+    width: 45vw;
+    font-size: 1.5vw;
+  }
 `;
 
 const Category = ({ image, text, redirectCategory }) => {
@@ -36,7 +56,7 @@ const Category = ({ image, text, redirectCategory }) => {
       <Box onClick={handleClick}>
         <img src={image} width={'100%'} height={'80%'} />
         <Text>{text}</Text>
-        <span>자세한 정보를 확인 하고 싶으시면 클릭을 해주세요</span>
+        <ExtraText>자세한 정보를 확인 하고 싶으시면 클릭을 해주세요</ExtraText>
       </Box>
     </>
   );

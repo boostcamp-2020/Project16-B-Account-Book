4603108fac4a3bf71cc8d577b6a38d047ca809d0
Yejikwon <yeeji77@naver.com>
2020-12-17 17:10:36 +0900

    [FE] refactor: #123 setting reducer store에 등록 & 영향받는 container state 정보 수정
    setting reducer를 store에 등록했습니다.

--- client/src/store.js
+++ client/src/store.js
@@ -3,11 +3,13 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import reducer from './slice';
 import paymentSlice from './paymentSlice';
 import calendarSlice from './calendarSlice';
+import settingSlice from './settingSlice';
 
 const rootReducer = combineReducers({
   default: reducer,
   payment: paymentSlice.reducer,
   calendar: calendarSlice.reducer,
+  setting: settingSlice.reducer,
 });
 
 const store = configureStore({

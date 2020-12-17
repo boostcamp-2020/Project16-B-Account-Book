923449a5d0dc8c4602ed5f92b529a4bdcc8bbe66
gyim1345 <gyim1345@gmail.com>
2020-12-17 17:13:39 +0900

    [FE] feat: transaction 용 slice 추가
    state slice에 넣기 위해서 slice 추가

---
+++ client/src/transactionSlice.js
@@ -0,0 +1,74 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const transactionSlice = createSlice({
+  name: 'transaction',
+  initialState: {
+    deleteStatus: false,
+    editIdStatus: '',
+    openModalStatus: false,
+    parserStatus: false,
+    bulkInsert: [],
+  },
+  reducers: {
+    setDeleteStatus(state, { payload: deleteStatus }) {
+      return {
+        ...state,
+        deleteStatus,
+      };
+    },
+
+    setEditIdStatus(state, { payload: editIdStatus }) {
+      return {
+        ...state,
+        editIdStatus,
+      };
+    },
+
+    setOpenModalStatus(state, { payload: openModalStatus }) {
+      return {
+        ...state,
+        openModalStatus,
+      };
+    },
+
+    setParserStatus(state, { payload: parserStatus }) {
+      return {
+        ...state,
+        parserStatus,
+      };
+    },
+
+    setBulkInsert(state, { payload: bulkInsert }) {
+      return {
+        ...state,
+        bulkInsert,
+      };
+    },
+  },
+});
+
+export const {
+  setDeleteStatus,
+  setEditIdStatus,
+  setOpenModalStatus,
+  setParserStatus,
+  setBulkInsert,
+} = transactionSlice.actions;
+
+export const closeModal = () => {
+  return (dispatch) => {
+    dispatch(setOpenModalStatus(false));
+    dispatch(setEditIdStatus(''));
+    dispatch(setParserStatus(false));
+    dispatch(setBulkInsert([]));
+  };
+};
+
+export const changeDeleteStatus = (status) => {
+  console.log(status, 'ㅁㄴㅇㅁㄴㅇ');
+  return (dispatch) => {
+    dispatch(setDeleteStatus(status));
+  };
+};
+
+export default transactionSlice;
